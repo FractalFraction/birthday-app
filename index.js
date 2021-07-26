165c79ef26bd79a9d32340d3325360acff1d62f9
Colin <cln.gardiner@outlook.com>
2021-07-26 16:41:04 +0100

    added form element

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+const app = express();
+const port = 3000;
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  //res.send('Hello!')
-  console.log('Rendering ejs to the page!')
+  //  res.send('Hello!')
+  console.log('Rendering ejs to the page!');
   res.render('index');
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
